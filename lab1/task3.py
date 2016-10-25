a796ffe8af349e91c7eb3ad162c28958bb9a9b11
Rafał Łasocha <me@swistak35.com>
2016-10-25 13:04:59 +0200

    Updated task 3

--- lab1/task3.py
+++ lab1/task3.py
@@ -5,13 +5,16 @@ import os.path
 
 def loadCalibrationMatrix(f):
     rows = []
-    for i in range(0,3): # 4 lines
+    for i in range(0,3): # 3 lines
         line = f.readline().split()
         rows.append([float(i) for i in line])
-    last_line = f.readline().split()
-    last_column = np.array([[float(i)] for i in last_line])
-    result = np.array(rows)
-    return np.concatenate((result, last_column), axis=1)
+    rotationMtx = np.array(rows)
+    translationVector = np.array([float(i) for i in f.readline().split()])
+    return (rotationMtx, translationVector)
+
+def buildProjectionMatrix(rotationMtx, translationVector):
+    translationVecAsColumn = np.array([[i] for i in translationVector])
+    return np.concatenate((rotationMtx, translationVecAsColumn), axis=1)
 
 def loadCalibData(filename):
     f = open(filename,'r')
@@ -22,21 +25,17 @@ def loadCalibData(filename):
         [0.0, 0.0, 1.0]
         ])
     f.readline()
-    f.readline()
-    f.readline()
-
-    mtx1 = loadCalibrationMatrix(f)
-    f.readline()
-    mtx2 = loadCalibrationMatrix(f)
-    f.readline()
-    mtx3 = loadCalibrationMatrix(f)
-    f.readline()
-    mtx4 = loadCalibrationMatrix(f)
-    f.readline()
-    mtx5 = loadCalibrationMatrix(f)
+    distortion = [ float(k) for k in f.readline().split() ]
     f.readline()
 
-    return [intrinsicMtx, mtx1, mtx2, mtx3, mtx4, mtx5]
+    mtxs = []
+    for i in range(0,5): # 5 matrices
+        rotMtx, tVec = loadCalibrationMatrix(f)
+        f.readline()
+        projectionMatrix = buildProjectionMatrix(rotMtx, tVec)
+        mtxs.append(intrinsicMtx.dot(projectionMatrix))
+
+    return (distortion, mtxs)
 
 def loadModelPoints(filename):
     points = []
@@ -50,7 +49,6 @@ def loadModelPoints(filename):
             points.append([float(line[6]), float(line[7])])
     return points
 
-
 def drawSmallCircle(draw, x, y):
     # Top left and bottom right corners
     colorPoint = 256
@@ -59,21 +57,65 @@ def drawSmallCircle(draw, x, y):
     draw.ellipse((x - width, y - width, x + width, y + width), fill = colorCircle)
     draw.point((x, y), fill = colorPoint)
 
-def drawPoints(filename, points):
+def drawPoints(filename, points, suffix = "-withpoints"):
     im = Image.open(filename)
     draw = ImageDraw.Draw(im)
-    drawSmallCircle(draw, 100, 100)
+    # drawSmallCircle(draw, 100, 100)
     for point in points:
         drawSmallCircle(draw, point[0], point[1])
     basePath, extPath = os.path.splitext(filename)
-    im.show(draw)
-    # im.save(basePath + "-withpoints" + extPath, "GIF")
+    # im.show(draw)
+    im.save(basePath + suffix + extPath, "GIF")
+
+def savePictureWithPoints(filename, projectionMatrix, homopoints):
+    projectedHomopoints = [ projectionMatrix.dot(p) for p in homopoints ]
+    projectedPoints = [ (hp[0] / hp[2], hp[1] / hp[2]) for hp in projectedHomopoints ]
+    drawPoints(filename, projectedPoints)
+
+# http://wiki.icub.org/wiki/Image_Coordinate_Standard
+def normalizedPoint(p, imageSize):
+    (w, h) = imageSize
+    (u, v) = p
+    return (2*u / w - 1, 2*v / h - 1)
+
+def denormalizedPoint(p, imageSize):
+    (w, h) = imageSize
+    (x, y) = p
+    return (w*(x+1) / 2, h*(y+1) / 2)
+
+def correctedPoint(p, distortion):
+    k1, k2 = distortion
+    r2 = p[0]*p[0] + p[1]*p[1]
+    # coefficient = 1 + k1*r2 + k2*r2*r2
+    # return (p[0]*coefficient, p[1]*coefficient)
+    return (p[0] + p[0] * k1 * r2 + p[0] * k2 * r2 * r2, p[1] + p[1] * k1 * r2 + p[1] * k2 * r2 * r2)
+
+def savePictureWithCorrectedPoints(filename, projectionMatrix, homopoints, distortion):
+    imageSize = (640, 480)
+    doubleHomopoints = [np.longdouble(p) for p in homopoints]
+    projectedHomopoints = [ projectionMatrix.dot(p) for p in doubleHomopoints ]
+    projectedPoints = [ (hp[0] / hp[2], hp[1] / hp[2]) for hp in projectedHomopoints ]
+    normalizedPoints = [ normalizedPoint(p, imageSize) for p in projectedPoints ]
+    correctedPoints = [ correctedPoint(p, distortion) for p in normalizedPoints ]
+    denormalizedPoints = [ denormalizedPoint(p, imageSize) for p in correctedPoints ]
+    drawPoints(filename, denormalizedPoints, suffix = "-correctedpoints")
 
 def run():
-    [intr, mtx1, mtx2, mtx3, mtx4, mtx5] = task3.loadCalibData("data/task34/Calib.txt")
-    points = task3.loadModelPoints("data/task34/Model.txt")
+    distortion, mtxs = loadCalibData("data/task34/Calib.txt")
+    points = loadModelPoints("data/task34/Model.txt")
     homopoints = [ (p[0], p[1], 0.0, 1.0) for p in points ]
-    p1 = intr.dot(mtx1)
-    homopoints1 = [ p1.dot(p) for p in homopoints ]
-    points1 = [ (hp[0] / hp[2], hp[1] / hp[2]) for hp in homopoints1 ]
-    drawPoints("")
+
+    # Undistorted pictures
+    savePictureWithPoints("data/task34/UndistortIm1.gif", mtxs[0], homopoints)
+    savePictureWithPoints("data/task34/UndistortIm2.gif", mtxs[1], homopoints)
+
+    # Distorted pictures
+    savePictureWithPoints("data/task34/CalibIm1.gif", mtxs[0], homopoints)
+    savePictureWithPoints("data/task34/CalibIm2.gif", mtxs[1], homopoints)
+    savePictureWithPoints("data/task34/CalibIm3.gif", mtxs[2], homopoints)
+    savePictureWithPoints("data/task34/CalibIm4.gif", mtxs[3], homopoints)
+    savePictureWithPoints("data/task34/CalibIm5.gif", mtxs[4], homopoints)
+
+    savePictureWithCorrectedPoints("data/task34/CalibIm1.gif", mtxs[0], homopoints, distortion)
+
+run()
