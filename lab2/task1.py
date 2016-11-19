111262b329694bd2b8a7d04c94dac41c6166224d
Rafał Łasocha <me@swistak35.com>
2016-11-19 21:19:49 +0100

    Fixed Task 1,2,3 and 4 from Lab2

--- lab2/task1.py
+++ lab2/task1.py
@@ -24,14 +24,18 @@ def drawSmallCircle(draw, x, y, colorCircle = (0, 255, 0), width = 2):
 def drawEpilines(filename, fundamentalMtx, points1, points2, suffix):
     im = Image.open(filename)
     draw = ImageDraw.Draw(im)
+    i = 0
     for (p1, p2) in zip(points1, points2):
+        lineColor = (200, 0, 240) if i < AMOUNT_OF_POINTS_TO_ESTIMATE else (200, 240, 0)
+        circleColor = (0, 0, 240) if i < AMOUNT_OF_POINTS_TO_ESTIMATE else (0, 240, 0)
         l = fundamentalMtx.dot(p2)
         x1 = 0
         y1 = (-l[2] - l[0] * x1) / l[1]
         x2 = im.size[0]
         y2 = (-l[2] - l[0] * x2) / l[1]
-        draw.line([(x1, y1), (x2, y2)], fill = 200, width = 3)
-        drawSmallCircle(draw, p1[0], p1[1], width = 4)
+        draw.line([(x1, y1), (x2, y2)], fill = lineColor, width = 3)
+        drawSmallCircle(draw, p1[0], p1[1], colorCircle = circleColor, width = 4)
+        i += 1
     im.save(mkPath(filename, suffix), "JPEG")
 
 def correctFundamental(f):
@@ -59,64 +63,78 @@ def getNormalizationMtx(points):
     r = mS.dot(mT)
     pointsd = r.dot(points.T).T
     avgDist = np.mean([ np.sqrt(x * x + y * y) / (w*w) for (x,y,w) in pointsd ])
-    print avgDist
     return r
 
 def calculateFundamentalMtx(points1, points2):
     points = np.hstack((points1, points2))[0:AMOUNT_OF_POINTS_TO_ESTIMATE]
-    # Czy to dobra kolejnosc ?
-    a = np.array([ (x1 * x2, x1 * y2, x1, y1 * x2, y1 * y2, y1, x2, y2, 1) for (x1, y1, w1, x2, y2, w2) in points ])
+    # a = np.array([ (x1 * x2, x1 * y2, x1, y1 * x2, y1 * y2, y1, x2, y2, 1) for (x1, y1, w1, x2, y2, w2) in points ])
+    # a = np.array([ ( x * xp, y * xp, xp, x * yp, y * yp, yp, x, y, 1 ) for (x, y, w, xp, yp, wp) in points ])
+    a = np.array([ (x1 * x2, x1 * y2, x1, y1 * x2, y1 * y2, y1, x2, y2, 1) for (x2, y2, w2, x1, y1, w1) in points ])
     [l, s, r] = np.linalg.svd(a)
-    f = r[-1].reshape(3,3)
-    return f
+    f1 = r[-1].reshape(3,3)
+
+    return f1
 
 def rescale(f):
-    return f
-    # return (f / f[2,2])
+    # return f
+    return (f / f[2,2])
 
-# Why we adjust Essential matrix in different way, than fundamental matrix?
-
-def run():
+def loadPointsData():
     mat = sc.io.loadmat("data/compEx1data.mat")['x']
     xim1 = mat[0,0].T
     xim2 = mat[1,0].T
 
+    return (xim1, xim2)
+
+def findRandomPoints(pointsData):
+    xim1, xim2 = pointsData
+    pts_indices = np.random.choice(xim1.shape[0], AMOUNT_OF_POINTS_TO_DRAW, replace=False)
     # "Random"
-    # pts_indices = [3, 42, 123, 234, 456, 567, 678, 890, 1024]
-    # pts_indices = [3, 20, 42, 64, 123, 234, 256, 456, 567, 600, 678, 890, 930, 1024]
+    pts_indices = [1592,  526,  393, 1403,  433,  576, 1087,  429,  611,  530,  812, 1449,  651,  561, 1018]
 
-    # quite good on 1,2,4: [1190  138 1562  631  407 1790  791 1448  112 1482  185  251 1233 1096  793 1680 1149 1809 1294 1715  906 1533 1949  809  629 1806 1353 1092  932  333 775  998 1870 1823  106  990  730 1275 1449 1424]
-    # pts_indices = np.random.choice(xim1.shape[0], AMOUNT_OF_POINTS_TO_DRAW)
-    pts_indices = [ 262,  639, 1955, 1375,  558, 1464,   10, 1670, 1897,  731, 1937, 1504, 1748,  120,  906]
-    print pts_indices
+    return pts_indices
 
-    ### Task 1, 2
-    # Drawing lines for uncorrected fundamental matrix
+# Drawing lines for uncorrected fundamental matrix
+def task1(pointsData, pts_indices):
+    xim1, xim2 = pointsData
     points1 = xim1[pts_indices]
     points2 = xim2[pts_indices]
     fundamentalMtx = rescale(calculateFundamentalMtx(points1, points2))
-    drawEpilines("data/kronan1.JPG", fundamentalMtx, points1, points2, "-phase1")
-    drawEpilines("data/kronan2.JPG", fundamentalMtx.T, points2, points1, "-phase1")
+    drawEpilines("data/kronan1.JPG", fundamentalMtx.T, points1, points2, "-phase1")
+    drawEpilines("data/kronan2.JPG", fundamentalMtx, points2, points1, "-phase1")
 
-    # Drawing lines for corrected fundamental matrix
+# Drawing lines for corrected fundamental matrix
+def task2(pointsData, pts_indices):
+    xim1, xim2 = pointsData
+    points1 = xim1[pts_indices]
+    points2 = xim2[pts_indices]
+    fundamentalMtx = rescale(calculateFundamentalMtx(points1, points2))
     correctedFundamentalMtx = rescale(correctFundamental(fundamentalMtx))
-    drawEpilines("data/kronan1.JPG", correctedFundamentalMtx, points1, points2, "-phase2")
-    drawEpilines("data/kronan2.JPG", correctedFundamentalMtx.T, points2, points1, "-phase2")
+    drawEpilines("data/kronan1.JPG", correctedFundamentalMtx.T, points1, points2, "-phase2")
+    drawEpilines("data/kronan2.JPG", correctedFundamentalMtx, points2, points1, "-phase2")
 
-    ### Task 3
-    normMtx1 = getNormalizationMtx(points1)
-    normMtx2 = getNormalizationMtx(points2)
+def task3(pointsData, pts_indices):
+    xim1, xim2 = pointsData
+    points1 = xim1[pts_indices]
+    points2 = xim2[pts_indices]
+
+    normMtx1 = getNormalizationMtx(xim1)
+    normMtx2 = getNormalizationMtx(xim2)
 
     normalizedPoints1 = normMtx1.dot(points1.T).T
     normalizedPoints2 = normMtx2.dot(points2.T).T
 
-    normalizedFundamentalMtx = calculateFundamentalMtx(normalizedPoints1, normalizedPoints2)
-    denormalizedFundamentalMtx = rescale(correctFundamental(normMtx2.T.dot(normalizedFundamentalMtx).dot(normMtx1)))
+    normalizedFundamentalMtx = rescale(correctFundamental(calculateFundamentalMtx(normalizedPoints1, normalizedPoints2)))
+    denormalizedFundamentalMtx = rescale(normMtx2.T.dot(normalizedFundamentalMtx).dot(normMtx1))
 
-    drawEpilines("data/kronan1.JPG", denormalizedFundamentalMtx, points1, points2, "-phase3")
-    drawEpilines("data/kronan2.JPG", denormalizedFundamentalMtx.T, points2, points1, "-phase3")
+    drawEpilines("data/kronan1.JPG", denormalizedFundamentalMtx.T, points1, points2, "-phase3")
+    drawEpilines("data/kronan2.JPG", denormalizedFundamentalMtx, points2, points1, "-phase3")
 
-    ### Task 4
+def task4(pointsData, pts_indices):
+    xim1, xim2 = pointsData
+    points1 = xim1[pts_indices]
+    points2 = xim2[pts_indices]
+
     intrinsicMtx = sc.io.loadmat("data/compEx3data.mat")['K']
     invIntrinsicMtx = np.linalg.inv(intrinsicMtx)
 
@@ -126,7 +144,19 @@ def run():
     essentialMtx = correctEssential(calculateFundamentalMtx(normalizedPoints1, normalizedPoints2))
     fundamentalMtxFromEssential = rescale(invIntrinsicMtx.T.dot(essentialMtx).dot(invIntrinsicMtx))
 
-    drawEpilines("data/kronan1.JPG", fundamentalMtxFromEssential, points1, points2, "-phase4")
-    drawEpilines("data/kronan2.JPG", fundamentalMtxFromEssential.T, points2, points1, "-phase4")
+    drawEpilines("data/kronan1.JPG", fundamentalMtxFromEssential.T, points1, points2, "-phase4")
+    drawEpilines("data/kronan2.JPG", fundamentalMtxFromEssential, points2, points1, "-phase4")
+
+def run():
+    pointsData = loadPointsData()
+    pts_indices = findRandomPoints(pointsData)
+
+    task1(pointsData, pts_indices)
+    task2(pointsData, pts_indices)
+    task3(pointsData, pts_indices)
+    task4(pointsData, pts_indices)
+
+    ### Task 5
+    # P1 = np.hstack((np.diag((1,1,1)), np.zeros(3).reshape(3,1)))
 
 run()
