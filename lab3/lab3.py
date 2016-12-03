52fb98f6593c23a522e03bb3eef443141a90d637
Rafał Łasocha <me@swistak35.com>
2016-12-03 22:12:57 +0100

    improvement lab3

--- lab3/lab3.py
+++ lab3/lab3.py
@@ -8,6 +8,12 @@ from matplotlib import pyplot as plt
 
 np.set_printoptions(threshold=np.nan)
 
+DEBUG = True
+
+def debug(arg):
+    if DEBUG:
+        print(arg)
+
 def KRTfromP(P):
     "given P decomposes it into K,R, and T"
     K, R = sc.linalg.rq(P[:,0:-1])
@@ -28,7 +34,23 @@ def mkPath(filename, suffix):
 def matrix_applier(coords, mtx):
     homor = mtx.dot(np.array([coords[1], coords[0], 1.0]))
     homor = homor / homor[2]
-    return (homor[1], homor[0])
+    return (homor[1] - 8.0, homor[0] - 154.0)
+
+def computeNeededShape(imageShape, mtx):
+    (height, width) = imageShape
+    print(imageShape)
+    cornerTopLeft = np.asarray([0.0, 0.0, 1.0])
+    cornerTopRight = np.asarray([width, 0.0, 1.0])
+    cornerBottomLeft = np.asarray([0.0, height, 1.0])
+    cornerBottomRight = np.asarray([width, height, 1.0])
+    pts = np.array([ cornerTopLeft, cornerTopRight, cornerBottomLeft, cornerBottomRight ])
+    pts = mtx.dot(pts.T).T
+    pts = pts[:,0:2] / pts[:,2:3]
+    # xMin, yMin = np.amin(pts, axis=0)
+    # xMax, yMax = np.amax(pts, axis=0)
+    xSpan, ySpan = np.ptp(pts, axis=0)
+
+    return (ySpan.astype(np.int32), xSpan.astype(np.int32))
 
 def rectifyImage(T1):
 
@@ -52,6 +74,10 @@ def rectifyImage(T1):
     transformedHomoR = T1.dot(homor.T).T
     heteroPoints = from2Homogenous(transformedHomoR)
 
+    newImageShape = computeNeededShape(imageShape, T1)
+    print("newImageShape")
+    print(newImageShape)
+
     xMin, yMin = np.amin(heteroPoints, axis = 0)
     xMax, yMax = np.amax(heteroPoints, axis = 0)
     print(xMin, yMin)
@@ -62,21 +88,27 @@ def rectifyImage(T1):
 
     # scipy.ndimage.interpolation.h)
 
-    cx,cy = np.meshgrid(np.arange(800), np.arange(650))
+    cx,cy = np.meshgrid(np.arange(newImageShape[1]), np.arange(newImageShape[0]))
     r = np.stack((cx,cy), axis=2).transpose((1, 0, 2)).reshape((-1,2))
     r = np.fliplr(r)
     # homor = toHomogenous(r)
 
-    shape = (650, 950)
-    o = np.arange(shape[0] * shape[1]).reshape(shape)
+    o = np.arange(newImageShape[0] * newImageShape[1]).reshape(newImageShape)
+
 
     T1inv = np.linalg.inv(T1)
     print(imager.shape)
-    mappedPointsR = sc.ndimage.interpolation.geometric_transform(imager, matrix_applier, output = o, output_shape = shape, extra_arguments=(T1inv,))
+    mappedPointsR = sc.ndimage.interpolation.geometric_transform(imager, matrix_applier, output = o, output_shape = newImageShape, extra_arguments=(T1inv,))
     print(mappedPointsR)
 
     coords = (0, 0)
     homor = T1inv.dot(np.array([coords[1], coords[0], 1.0]))
+    homor = homor / homor[2]
+    print ("== (0,0) transformed to:")
+    print (homor[1], homor[0])
+
+    coords = (0, 0)
+    homor = T1.dot(np.array([coords[1], coords[0], 1.0]))
     homor = homor / homor[2]
     print ("== (0,0) transformed to:")
     print (homor[1], homor[0])
@@ -111,26 +143,33 @@ def rectify(Po1Tld, Po2Tld):
     Ko1, Ro1, To1 = KRTfromP(Po1Tld)
     Ko2, Ro2, To2 = KRTfromP(Po2Tld)
 
-    r1 = (c1 - c2) / np.linalg.norm(c1 - c2)
+    r1 = (c2 - c1) / np.linalg.norm(c2 - c1)
     r2 = np.cross(Ro1[2], r1)
     r3 = np.cross(r1, r2)
     Rn = np.array([ r1, r2, r3 ])
 
+    Kn = (Ko1 + Ko2) / 2 # Isn't that enough? Why do we still need vertical displacement?
+    # Kn = Ko1
+    Kn[0,1] = 0.0
+
     Tn1 = -Rn.dot(c1)
     Tn2 = -Rn.dot(c2)
-    Pn1 = Ko1.dot(np.hstack((Rn, Tn1.reshape(3,1))))
-    Pn2 = Ko1.dot(np.hstack((Rn, Tn2.reshape(3,1))))
+    Pn1 = Kn.dot(np.hstack((Rn, Tn1.reshape(3,1))))
+    Pn2 = Kn.dot(np.hstack((Rn, Tn2.reshape(3,1))))
 
     T1 = Pn1[:,0:3].dot(np.linalg.inv(Po1))
     T2 = Pn2[:,0:3].dot(np.linalg.inv(Po2))
 
-    print("Distance between two camera centres:")
-    baseline = np.linalg.norm(Tn1 - Tn2)
-    print(baseline)
-    print(Ko1)
-    print(Ko2)
-    focalX = Ko1[0,0]
-    focalY = Ko1[1,1]
+    # print("Distance between two camera centres:")
+    # baseline = np.linalg.norm(Tn1 - Tn2)
+    # print(baseline)
+    # print(Ko1)
+    # print(Ko2)
+    # focalX = Ko1[0,0]
+    # focalY = Ko1[1,1]
+
+    # print("Skew: %f" % Ko1[0,1])
+
 
     return (Pn1, Pn2, T1, T2)
 
@@ -196,30 +235,48 @@ def task3():
     # cv2.imshow('disparity', disp.astype(np.float32) / np.amax(disp))
     # cv2.waitKey()
 
-    # imgL = cv2.imread("data/rectified/Sport_R_0.png", 1)
-    # imgR = cv2.imread("data/rectified/Sport_R_1.png", 1)
+    imgL = cv2.imread("data/rectified/Sport_R_0.png", 1)
+    imgR = cv2.imread("data/rectified/Sport_R_1.png", 1)
 
     stereo = cv2.StereoSGBM_create(
         minDisparity = 32,
         numDisparities = 144,
         blockSize = 13,
-        # P1 = 4*13**2,
-        # P2 = 16*13**2,
+        P1 = 2048,
+        P2 = 8192,
         # disp12MaxDiff = 1,
         # uniquenessRatio = 10,
-        speckleWindowSize = 75,
-        speckleRange = 1,
+        speckleWindowSize = 125,
+        speckleRange = 3,
         # fullDP = True
     )
     disp = stereo.compute(imgL, imgR)
-    print(np.amin(disp))
-    print(np.amax(disp))
+
+    imgL = cv2.imread("data/rectified/Sport_R_0.png", 1)
+    imgR = cv2.imread("data/rectified/Sport_R_1.png", 1)
+
+    stereo2 = cv2.StereoSGBM_create(
+        minDisparity = 32,
+        numDisparities = 144,
+        blockSize = 13,
+        P1 = 2048,
+        P2 = 8192,
+        # disp12MaxDiff = 1,
+        # uniquenessRatio = 10,
+        speckleWindowSize = 125,
+        speckleRange = 3,
+        # fullDP = True
+    )
+    disp2 = stereo2.compute(imgL, imgR)
+    # print(np.amin(disp))
+    # print(np.amax(disp))
     cv2.imshow('disparity', disp.astype(np.float32) / np.amax(disp))
+    cv2.imshow('disparity2', disp2.astype(np.float32) / np.amax(disp2))
     cv2.waitKey()
 
 
 def run():
-    task12()
-    # task3()
+    # task12()
+    task3()
 
 run()
