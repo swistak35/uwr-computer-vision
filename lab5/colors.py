e72cd97c87b870f52806d5f678e1f4b4d7ce8306
Rafał Łasocha <me@swistak35.com>
2017-01-31 01:44:38 +0100

    Refactoring
    Also:
    * add computing orientation
    * add verification stats

--- lab5/colors.py
+++ lab5/colors.py
@@ -1,4 +1,5 @@
 import random
+import numpy as np
 
 COLORS = {
     'RED':    (255, 0,   0),
@@ -7,7 +8,24 @@ COLORS = {
     'DUNNO1': (0,   255, 255),
     'DUNNO2': (255, 0,   255),
     'DUNNO3': (255, 255, 0),
+    'BLACK':  (0, 0, 0),
 }
 
 def getRandomColor():
     return random.choice(COLORS.values())
+
+def drawCircle(draw, x, y, fillColor = None, outlineColor = COLORS['BLACK'], width = 2):
+    # Top left and bottom right corners
+    draw.ellipse((x - width, y - width, x + width, y + width), fill = fillColor, outline = outlineColor)
+
+def drawLineUnderAngle(draw, x, y, length, angle):
+    draw.line([
+        (x, y),
+        (x + length * np.cos(np.radians(angle)), y + length * np.sin(np.radians(angle)))
+    ])
+
+DEBUG = False
+
+def debug(msg):
+    if DEBUG:
+        print("[DEBUG] %s" % msg)
--- lab5/matching.py
+++ lab5/matching.py
@@ -4,7 +4,7 @@ import numpy as np
 import os.path
 import PIL as pil
 import Image, ImageDraw
-from colors import getRandomColor, COLORS
+from colors import getRandomColor, COLORS, debug, drawCircle, drawLineUnderAngle
 
 SIGMA = 1.6
 OCTAVES = 4
@@ -13,48 +13,34 @@ SIGMA_K = np.power(2, 1.0 / SCALES_PER_OCTAVE)
 
 SANITY_THRESHOLD = 3.0
 
-
 # TODO: There should be an option to display only "50 best" matches
 
 class FeatureMatching:
-    def drawSmallCircle(self, draw, x, y, fillColor = None, outlineColor = COLORS['GREEN'], width = 2):
-        # Top left and bottom right corners
-        draw.ellipse((x - width, y - width, x + width, y + width), fill = fillColor, outline = outlineColor)
+    def __init__(self):
+        self.matches = None
 
-    def featureForDrawing(self, draw, x, y, scale, octave, angle, offset):
+    # Drawing
+    def featureForDrawing(self, f, offset):
+        (y, x, octave, scale, angle) = f
         realX = x * np.power(2, octave) + offset
         realY = y * np.power(2, octave)
         radius = 3 + SIGMA * np.power(SIGMA_K, scale) * np.power(2, octave)
         angleDeg = np.degrees(angle)
         return (realX, realY, radius, angleDeg)
-        # maxPeakV = peaks[0][1]
-        # if maxPeakV != 0: # how is that possible that there are zeros?
-        #     normalizedPeaks = [ (i, v / maxPeakV) for (i,v) in peaks ]
-        #     for (peakIndex, peakValue) in normalizedPeaks:
-        #         lineLength = radius * peakValue
-        #         angle = peakIndex * ORIENTATION_BIN_ANGLE
-        #         draw.line([
-        #             (realX, realY),
-        #             (realX + lineLength * np.cos(np.radians(angle)), realY + lineLength * np.sin(np.radians(angle)))
-        #         ])
 
     def drawMatch(self, draw, m, offset):
-        (y1, x1, octave1, scale1, currentAngle1) = m[0][0:5]
-        (y2, x2, octave2, scale2, currentAngle2) = m[1][0:5]
-        (rx1, ry1, radius1, angle1) = self.featureForDrawing(draw, x1, y1, scale1, octave1, currentAngle1, 0)
-        (rx2, ry2, radius2, angle2) = self.featureForDrawing(draw, x2, y2, scale2, octave2, currentAngle2, offset)
+        (rx1, ry1, radius1, angle1) = self.featureForDrawing(m[0][0:5], 0)
+        (rx2, ry2, radius2, angle2) = self.featureForDrawing(m[1][0:5], offset)
         color = getRandomColor()
-        self.drawSmallCircle(draw, rx1, ry1, width = radius1, outlineColor = color)
-        self.drawSmallCircle(draw, rx2, ry2, width = radius2, outlineColor = color)
-        # TODO: Draw orientation
-        draw.line([
-                (rx1, ry1),
-                (rx2, ry2),
-            ], fill = color, width = 1)
+        drawCircle(draw, rx1, ry1, width = radius1, outlineColor = color)
+        drawLineUnderAngle(draw, rx1, ry1, radius1, angle1)
+        drawCircle(draw, rx2, ry2, width = radius2, outlineColor = color)
+        drawLineUnderAngle(draw, rx2, ry2, radius2, angle2)
+        draw.line([(rx1, ry1), (rx2, ry2)], fill = color, width = 1)
 
-    def drawMatches(self, imageFilename1, imageFilename2, matches):
-        im1 = Image.open(imageFilename1)
-        im2 = Image.open(imageFilename2)
+    def drawMatches(self, outputFilename):
+        im1 = Image.open(self.imageFilename1)
+        im2 = Image.open(self.imageFilename2)
         totalWidth = im1.size[0] + im2.size[0]
         maxHeight = max(im1.size[1], im2.size[1])
         imOut = Image.new('RGB', (totalWidth, maxHeight))
@@ -62,22 +48,20 @@ class FeatureMatching:
         imOut.paste(im1, (0, 0))
         imOut.paste(im2, (offset, 0))
         draw = ImageDraw.Draw(imOut)
-        for m in matches:
+        for m in self.matches:
             self.drawMatch(draw, m, offset)
-        # TODO: Don't write to the same file
-        imOut.save("matches.jpg", "JPEG")
+        imOut.save(outputFilename, "JPEG")
 
-    def matchImages(self, imageFilename1, imageFilename2, verificationFile):
+    def compute(self, imageFilename1, imageFilename2):
+        self.imageFilename1 = imageFilename1
+        self.imageFilename2 = imageFilename2
+
         basePath1, extPath1 = os.path.splitext(imageFilename1)
         basePath2, extPath2 = os.path.splitext(imageFilename2)
         matFilename1 = basePath1 + "-features.mat"
         matFilename2 = basePath2 + "-features.mat"
         features1 = sc.io.loadmat(matFilename1)['features']
         features2 = sc.io.loadmat(matFilename2)['features']
-
-        if verificationFile:
-            mats = sc.io.loadmat(verificationFile)
-            verifiedFeatures = np.hstack((mats['x1'], mats['y1'], mats['x2'], mats['y2']))
 
         matches = []
         for fi, f in enumerate(features1):
@@ -96,25 +80,58 @@ class FeatureMatching:
             print("Found match for %d, value: %f" % (fi,bestValue))
             matches.append((f, features2[bestIdx]))
 
-            if verificationFile:
-                norms = np.linalg.norm(verifiedFeatures[:, 0:2] - (x, y), axis = 1)
-                foundNorms = verifiedFeatures[norms < 5.0]
-                if foundNorms.shape[0] == 0:
-                    print("Verification: didn't find this feature in verification table")
-                else:
-                    f2 = (features2[bestIdx][1], features2[bestIdx][0])
-                    results = np.linalg.norm(foundNorms[:, 2:4] - f2, axis=1)
-                    verifiedResults = foundNorms[results < 5.0]
-                    if foundNorms.shape[0] == 1:
-                        if verifiedResults.shape[0] == 1:
-                            print("Verification: Found and verified exactly one feature")
-                        else:
-                            print("Verification: Found exactly one feature, but didn't verified it (distance: %f)" % (results[0]))
-                    else:
-                        print("Verification: Found multiple features")
 
         print("Found %d matches" % (len(matches)))
-        self.drawMatches(imageFilename1, imageFilename2, matches)
+        self.matches = matches
+
+    def verify(self, verificationFile):
+        mats = sc.io.loadmat(verificationFile)
+        verifiedFeatures = np.hstack((mats['x1'], mats['y1'], mats['x2'], mats['y2']))
+
+        verifiedCounter = 0
+        verifiedFailedCounter = 0
+        notFoundCounter = 0
+        otherCounter = 0
+        totalCounter = len(self.matches)
+
+        for (f1, f2) in self.matches:
+            (y1, x1) = f1[0:2]
+            (y2, x2) = f2[0:2]
+
+            norms = np.linalg.norm(verifiedFeatures[:, 0:2] - (x1, y1), axis = 1)
+            foundNorms = verifiedFeatures[norms < 5.0]
+            if foundNorms.shape[0] == 0:
+                notFoundCounter += 1
+                debug("Verification: didn't find this feature in verification table")
+            else:
+                results = np.linalg.norm(foundNorms[:, 2:4] - (x2, y2), axis=1)
+                verifiedResults = foundNorms[results < 5.0]
+                if foundNorms.shape[0] == 1:
+                    if verifiedResults.shape[0] == 1:
+                        verifiedCounter += 1
+                        debug("Verification: Found and verified exactly one feature")
+                    else:
+                        verifiedFailedCounter += 1
+                        debug("Verification: Found exactly one feature, but didn't verified it (distance: %f)" % (results[0]))
+                else:
+                    otherCounter += 1
+                    debug("Verification: Found multiple features")
+
+        print("=== Verification complete.")
+        print("Verified:  %d" % verifiedCounter)
+        print("Failed:    %d" % verifiedFailedCounter)
+        print("Not found: %d" % notFoundCounter)
+        print("Other:     %d" % otherCounter)
+        print("Total:     %d" % totalCounter)
+
+
+
+
+
+
+
+
+
 
 
 def run():
@@ -122,15 +139,13 @@ def run():
             ("data/Notre Dame/1_o.jpg", "data/Notre Dame/2_o.jpg", "data/Notre Dame/921919841_a30df938f2_o_to_4191453057_c86028ce1f_o.mat"),
             # ("data/duda/img_20170130_162706.jpg", "data/duda/c3bxl_zweaywcbm.jpg", None),
             # ("data/fountain/0000.png", "data/fountain/0001.png", None),
-            # ("data/Mount Rushmore/9021235130_7c2acd9554_o.jpg", "data/Mount Rushmore/9021235130_7c2acd9554_o-featuresmat.mat"),
-            # ("data/Mount Rushmore/9318872612_a255c874fb_o.jpg","data/Mount Rushmore/9318872612_a255c874fb_o-featuresmat.mat"),
-            # ("data/Episcopal Gaudi/3743214471_1b5bbfda98_o.jpg", "data/Episcopal Gaudi/3743214471_1b5bbfda98_o-featuresmat.mat"),
-            # ("data/Episcopal Gaudi/4386465943_8cf9776378_o.jpg", "data/Episcopal Gaudi/4386465943_8cf9776378_o-featuresmat.mat"),
         ]
 
     for fileset in filesets:
         print("=== Files: (%s, %s)" % fileset[0:2])
         fm = FeatureMatching()
-        fm.matchImages(fileset[0], fileset[1], fileset[2])
+        fm.compute(fileset[0], fileset[1])
+        fm.drawMatches("matches.jpg")
+        fm.verify(fileset[2])
 
 run()
